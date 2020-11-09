9c65e4dcbce6f40dd3596b75d1d42e7ff73d0f68
Jose <j.a.ojeda@live.com>
2020-11-08 19:24:33 -0800

    added cors

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders')
 
 app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
